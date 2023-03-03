202b4e4ab1369d9c50259760888337fb6f90b6a5
NataliiaRud <nrud1981@gmail.com>
2023-03-03 10:18:43 +0200

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+stopBtn.setAttribute('disabled', true);
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log(bodyEl);
